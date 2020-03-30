09111b12ab271d3b23e2ff5b5a90a805eb77c182
Vishwal P K <vishwalpk3@gmail.com>
2020-03-30 19:46:54 -0400

    app: Allowing build for github-pages

---
+++ app/build/precache-manifest.342db11b71cd0bb0514ebe757386faff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c81cde23394a4fc7fb69c220f06f4e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "882cffdb07d2f33b622a",
+    "url": "/static/css/main.761a2e0c.chunk.css"
+  },
+  {
+    "revision": "3a0a4743f05a33a84c8f",
+    "url": "/static/js/2.c7591ce3.chunk.js"
+  },
+  {
+    "revision": "a8fa0a685d8af2934ba8ade7768fed07",
+    "url": "/static/js/2.c7591ce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "882cffdb07d2f33b622a",
+    "url": "/static/js/main.d73a92d9.chunk.js"
+  },
+  {
+    "revision": "0c31d059b37996e08397",
+    "url": "/static/js/runtime-main.6f70b99a.js"
+  },
+  {
+    "revision": "72d7f9b229b04c7d743f811fa91c33ab",
+    "url": "/static/media/hero-graphic.72d7f9b2.svg"
+  },
+  {
+    "revision": "6d6637b91d3187ed45f975aeb42bd831",
+    "url": "/static/media/waves.6d6637b9.png"
+  }
+]);
