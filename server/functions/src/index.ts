09c37d6c0afd47d1852131f91d44c4d1d6bebb6c
Vishwal P K <vishwalpk3@gmail.com>
2020-03-31 23:41:46 -0400

    Server: Added postAnswers

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -8,13 +8,33 @@ import Question from "./Models/Question";
 export const getQuestions = functions.https.onRequest(
   async (request, response) => {
     const questions = await Question.get();
-    response.json(questions);
+    response.json(
+      questions.map(({ options, ...rest }) => ({
+        ...rest,
+        options: options.map(({ isAnswer, ...otherOptionFields }) => ({
+          ...otherOptionFields,
+        })),
+      })),
+    );
   },
 );
 
-// export const getAnswers = functions.https.onRequest(
-//   async (request, response) => {
-//     const { questions } = request.body;
-//     response.json(questions)
-//   },
-// );
+export const postAnswers = functions.https.onRequest(
+  async (request, response) => {
+    if (request.method !== "POST") {
+      response.status(400).json({
+        message: "Please send a POST request",
+      });
+      return;
+    }
+
+    const { questions: ids } = request.body;
+
+    const questions = await Question.get();
+    response.json(
+      questions
+        .filter(q => q._id in ids)
+        .map(q => ({ [q._id]: q.options.find(o => o.isAnswer) })),
+    );
+  },
+);
